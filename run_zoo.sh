c025ae4d3fd88a5540e655ff157f509d2fb53ab6
blacknoir <blacknoir@blacknoir>
2015-03-18 15:38:39 +0500

    .gitignore fix

--- run_zoo.sh
+++ run_zoo.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-MYIP=`curl $BOOTIP`
-
-export CLASSPATH="/opt/zookeeper/build/zookeeper-3.4.7-SNAPSHOT.jar" 
+MYIP=`curl $BOOTIP` 
 
 echo $ID_ENV > /opt/zookeeper/myid
 
@@ -11,4 +9,4 @@ echo "networkaddress.cache.negative.ttl = 0" >> $JAVA_HOME/jre/lib/security/java
 
 curl -X PUT -H "Content-Type: application/json" -d '{"ID": "zookeeper'$ID_ENV'", "Name": "zookeeper'$ID_ENV'", "Address": "'$MYIP'"}' http://$BOOTIP:8500/v1/agent/service/register
 
-cd /opt/zookeeper && /opt/zookeeper/bin/zkEnv.sh && /opt/zookeeper/bin/zkServer.sh start-foreground
+cd /opt/zookeeper && /opt/zookeeper/bin/zkServer.sh start-foreground
